205f3870619d00317e0b02effc8388a36961cc60
yogamable <nicovelbaz@gmail.com>
2024-04-18 09:59:46 -0400

    prueba-terminada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
+$("#boton-enviar").click(function(){
+    alert("Tu mail se ha enviado. ¡Gracias por ponerte en contacto con nosotros!");
+  });
